46ab0a2184639b9788ecbcc56c584a263ba6bb91
Tymon Tobolski <i@teamon.eu>
2010-10-27 15:19:16 +0200

    Added Logger class

---
+++ src/main/java/eu/teamon/volley/utils/Logger.java
@@ -0,0 +1,41 @@
+package eu.teamon.volley.utils;
+
+/**
+ * Utils class for easier logging
+ * 
+ * @author teamon
+ */
+public class Logger {
+    public static final int DEBUG = 0;
+    public static final int INFO = 1;
+    public static final int WARN = 2;
+    public static final int ERROR = 3;
+    
+    
+    private static int level = 0;
+    
+    public static void setLevel(int i){
+        level = i;
+    }
+    
+    public static void debug(String msg){
+        if(level <= DEBUG) log("DEBUG", msg);
+    } 
+    
+    public static void info(String msg){
+        if(level <= INFO) log("INFO", msg);
+    }
+    
+    public static void warn(String msg){
+        if(level <= WARN) log("WARN", msg);
+    }
+    
+    public static void error(String msg){
+        if(level <= ERROR) log("ERROR", msg);
+    }
+    
+    
+    private static void log(String type, String msg){
+        System.out.println("[" + type + "] " + msg);
+    }
+}
